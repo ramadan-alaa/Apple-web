eaae82b3d660457b00f0a807dc67ab065d272ade
Ali <ali-alaa@gmail.com>
2025-11-29 23:16:27 +0200

    Add: Implement animation utility for GSAP timeline in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModalView from "./ModalView";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
 import { color } from "three/tsl";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Modal = () => {
   const [size, setSize] = useState("small");
@@ -25,6 +26,23 @@ const Modal = () => {
 
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0%)",
+        duration: 2,
+      });
+    }
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+  }, [size]);
 
   useGSAP(() => {
     gsap.to("#heading", {
@@ -92,12 +110,16 @@ const Modal = () => {
                 ))}
               </ul>
               <button className="size-btn-container cursor-pointer">
-                {sizes.map(({ label, value}) => (
-                  <span key={label} className="size-btn"
-                  style={{
-                    backgroundColor: size === value ? "white" : "transparent",
-                    color : size === value ? "black" : "white",
-                  }} onClick={() => setSize(value)}>
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
                     {label}
                   </span>
                 ))}
